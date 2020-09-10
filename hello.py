23d27a26fdb67173954a066308186e39869179c8
sanjeev <sanjeevsagar013@gmail.com>
2020-09-10 19:20:10 +0530

    hello world

---
+++ hello.py
@@ -0,0 +1,11 @@
+#list
+fruit=['apple','mango','lichi']
+print((fruit)
+#add element in list
+fruit.append("dragon fruit")
+print(fruit)
+
+#remove remove
+fruit.remove('mango')
+fruit.insert(1,"orange")
+print(fruit)
