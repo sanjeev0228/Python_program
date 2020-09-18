14795cec7a723552a385b85765a710babc172a82
sanjeev <sanjeevsagar013@gmail.com>
2020-09-18 13:39:46 +0530

    list

---
+++ variable.py
@@ -0,0 +1,23 @@
+list = ['apple','banana','dragonfruit','grape']
+a=[1,3,4,5,6,]
+list.append(12)
+print(list)
+#insert value
+list.insert(245)
+print(list
+      #remove value
+list.remove('apple')
+print(list)
+# function in list
+
+print(max(list))
+print(min(list))
+# cmp for compare
+print (cmp)
+#compare through loop
+list1 = [1,2,3,4,5,6]
+list2 = [7,8,9,2,10]
+for x in list1:
+    for y in list2:
+        if x == y:
+            print("The common element is:",x)
