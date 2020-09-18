cdbfe5018ea7b29107c6e17fcbe1332b0b57d8fc
sanjeev <sanjeevsagar013@gmail.com>
2020-09-18 09:35:47 +0530

    tuple

---
+++ tuple.py
@@ -0,0 +1,13 @@
+"""
+tupple is similiar like list but is immutable
+"""
+t1=('sanjeev','devesh','vaibhav','utkarsh')
+t2=(12,'java',234,'c++',789,'python','php')
+t3=123,456,76
+print(type(t2))
+#indexing of tuple
+print(t1[1:3])
+# tuple negative  indexing
+print(t2[1:4:2])
+print(t3[-1:-2])
+#set
